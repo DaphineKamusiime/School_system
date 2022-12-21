e1334f03e7adca96410947a2f121a486dd00c5b3
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-12-21 03:19:15 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,38 +2,50 @@ import java.util.Scanner;
 public class Main {
       
     public static void main(String[] args){
-        Scanner obj=new Scanner(System.in);
+        
         System.out.println("Enter name");
+
         System.out.println("Enter age");
+       
         System.out.println("Enter year of entry");
+
         System.out.println("Enter course");
+
         System.out.println("Enter residence_status");
+        
         System.out.println("Enter time spent");
         
-        String name=obj.nextLine();
-        int age=obj.nextInt();
-        int year=obj.nextInt();
-        String course=obj.nextLine();
-        String residence=obj.nextLine();
-        int time_spent=obj.nextInt();
+        Scanner myobj=new Scanner(System.in);
 
-        System.out.print("NAME----");
-        System.out.println(name);
+        String name=myobj.nextLine();
 
-        System.out.print("AGE----");
-        System.out.println(age);
+        int age=myobj.nextInt();
 
-        System.out.print("YEAR----");
-        System.out.println(year);
+        myobj.nextLine();
 
-        System.out.print("COURSE----");
-        System.out.println(course);
+        int year=myobj.nextInt();
 
-        System.out.print("RESIDENCE STATUS---");
-        System.out.println(residence); 
+        myobj.nextLine();
 
-        System.out.print("time spent---");
-        System.out.println(time_spent); 
+        String course=myobj.nextLine();
+
+        String residence=myobj.nextLine();
+        
+        int time_spent=myobj.nextInt();
+        
+        myobj.nextLine();
+
+        System.out.print("NAME: " + name);
+
+        System.out.print("       AGE: "+ age );
+
+        System.out.print("   YEAR: " + year);
+        
+        System.out.print("    COURSE: " + course);
+      
+        System.out.print("   RESIDENCE: " + residence);
+       
+        System.out.print("   TIME: " + time_spent);
            
     }
 }
