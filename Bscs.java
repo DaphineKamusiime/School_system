431ff76f51d7cc7496ce7aa97b34e5055be54b5f
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-04-21 19:06:33 +0300

    Update Bscs.java

--- Bscs.java
+++ Bscs.java
@@ -2,10 +2,6 @@ public class Bscs extends Student{
     static void years (int time_spent){
         System.out.print("DURATION  ");
         System.out.println(time_spent);
-        System.out.println("Years");
-        
+        System.out.println("Years"); 
     }
-
-
 }
-   
